dd29fa0111ecfbc0afd8d036d78b12500ba4ede2
Truong Dang <94695575+2dtkingslayer@users.noreply.github.com>
2023-01-25 21:37:13 -0500

    upload js

---
+++ style/js/create.js
@@ -0,0 +1,8 @@
+/* Name: Truong Duc Dang
+Date: December 09, 2022
+Course/Section: IT 202 001
+Semester Project Phase 3 */
+
+function CheckPrice() {
+    alert("New books information received!");
+}
---
+++ style/js/home.js
@@ -0,0 +1,42 @@
+var replace = document.getElementById("index");
+console.log(replace);
+var increase = 4;
+
+function LoadMore() {
+  increase = 0;
+  let current = parseInt(document.getElementById("index").textContent);
+  console.log(current);
+  for (let i = current - 3; i <= current; i++) {
+    console.log("i: " + i);
+    let identity = "book" + i;
+    console.log(identity);
+    var each = document.getElementById(identity);
+    if (each != null) {
+      increase++;
+      each.style.display = "inline";
+    }
+
+    if (i >= 16) {
+      let booknew = document.getElementById("booknew" + i);
+      booknew.src = "style/images/book/booknew.jpg";
+    }
+  }
+  replace.innerHTML = (current + increase);
+}
+
+function a_clicked(number) {
+  var check = "popup" + number;
+  console.log("button has been clicked: book " + number);
+  var popupScreen = document.getElementById(check);
+  
+  popupScreen.classList.add("activescreen");
+  
+}
+
+function close_clicked(number) {
+  var check = "popup" + number;
+  console.log("close: book " + number);
+  var popupScreen = document.getElementById(check);
+  
+  popupScreen.classList.remove("activescreen");
+}
