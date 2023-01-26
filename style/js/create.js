5b90dc4cca02d9861aad827146846592f2be84c2
Truong Dang <94695575+2dtkingslayer@users.noreply.github.com>
2023-01-25 21:46:13 -0500

    Update create.js

--- style/js/create.js
+++ style/js/create.js
@@ -1,8 +1,15 @@
-/* Name: Truong Duc Dang
-Date: December 09, 2022
-Course/Section: IT 202 001
-Semester Project Phase 3 */
-
 function CheckPrice() {
     alert("New books information received!");
+}
+
+function CheckName() {
+    // update soon
+}
+
+function CheckAuthor() {
+    // update soon
+}
+
+function CheckDescription() {
+    // update soon
 }
